ec44a36e82d59a14ca68357730a458056c9ec59b
Guhyun <moing6862@naver.com>
2019-08-01 18:24:59 +0900

    ㅇ

--- resource.h
+++ resource.h
@@ -21,10 +21,12 @@
 #define IDB_BITMAP9                     137
 #define IDB_BITMAP_M1                   138
 #define IDB_BITMAP_M2                   139
+#define IDB_BITMAP_INGAME               154
 #define IDB_BITMAP_DEFAULTBG            200
 #define IDB_BITMAP_OPENINGBG            201
 #define IDB_BITMAP_INGAMEBG             202
 #define IDB_BITMAP_ENDINGBG             203
+#define IDB_BITMAP_MAPSAMPLE            300
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -32,7 +34,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        153
+#define _APS_NEXT_RESOURCE_VALUE        155
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           111
