367264cc299a192f4375f9de8128d506e4826356
AZIT_REMi_2018 <amorigeta+git@gmail.com>
2019-08-05 00:50:48 +0900

    리소스 추가

--- resource.h
+++ resource.h
@@ -25,8 +25,35 @@
 #define IDB_BITMAP_MLEFT3               162
 #define IDB_BITMAP_MLEFT4               163
 #define IDB_BITMAP_MLEFT5               164
-#define IDB_BITMAP_IDLE1                165
-#define IDB_BITMAP_IDLE2                166
+#define IDB_BITMAP_FALL1                168
+#define IDB_BITMAP_FALL2                169
+#define IDB_BITMAP_FALL3                170
+#define IDB_BITMAP_FALL4                171
+#define IDB_BITMAP_LEFTJUMP1            172
+#define IDB_BITMAP_LEFTJUMP2            173
+#define IDB_BITMAP_LEFTJUMP3            174
+#define IDB_BITMAP_LEFTJUMP4            175
+#define IDB_BITMAP_LEFTJUMP5            176
+#define IDB_BITMAP_LEFTJUMP6            177
+#define IDB_BITMAP_LEFTJUMP7            178
+#define IDB_BITMAP_LEFTJUMP8            179
+#define IDB_BITMAP_RIGHTJUMP1           180
+#define IDB_BITMAP_RIGHTJUMP2           181
+#define IDB_BITMAP_RIGHTJUMP3           182
+#define IDB_BITMAP_RIGHTJUMP4           183
+#define IDB_BITMAP_RIGHTJUMP5           184
+#define IDB_BITMAP_RIGHTJUMP6           185
+#define IDB_BITMAP_RIGHTJUMP7           186
+#define IDB_BITMAP_RIGHTJUMP8           187
+#define IDB_BITMAP_CLIMB1               190
+#define IDB_BITMAP_CLIMB2               191
+#define IDB_BITMAP_IDLE1                192
+#define IDB_BITMAP_IDLE2                193
+#define IDB_BITMAP_IDLE3                194
+#define IDB_BITMAP_IDLE4                195
+#define IDB_BITMAP5                     196
+#define IDB_BITMAP_IDLE5                196
+#define IDB_BITMAP_IDLE6                197
 #define IDB_BITMAP_DEFAULTBG            200
 #define IDB_BITMAP_OPENINGBG            201
 #define IDB_BITMAP_INGAMEBG             202
@@ -39,7 +66,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        167
+#define _APS_NEXT_RESOURCE_VALUE        198
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           111
